8c08db4c02fcc9973a88526d130bb339531124e3
Ismagac <ismaeldemostoles2014@gmail.com>
2025-03-01 23:03:25 +0100

    Refactor whitespace and formatting in sopaLetras.py for improved readability

--- sopaLetras.py
+++ sopaLetras.py
@@ -2,56 +2,59 @@ from typing import List
 
 class TrieNode:
     def __init__(self):
-        self.children = {} 
-        self.word = None 
+        self.children = {}
+        self.word = None
 
 class Trie:
     def __init__(self):
-        self.root = TrieNode()  
+        self.root = TrieNode()
 
     def insert(self, word: str):
-        node = self.root  
+        node = self.root
         for char in word:
             if char not in node.children:
-                node.children[char] = TrieNode()  
-            node = node.children[char] 
-        node.word = word 
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.word = word
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        trie = Trie() 
+        trie = Trie()
         for word in words:
-            trie.insert(word)  
+            trie.insert(word)
 
-        rows, cols = len(board), len(board[0]) 
-        result = []  
+        rows, cols = len(board), len(board[0])
+        result = []
 
         def dfs(r, c, node):
-            char = board[r][c] 
+            char = board[r][c]
             if char not in node.children:
                 return
-            
-            node = node.children[char] 
-            if node.word:  
+
+            node = node.children[char]
+            if node.word:
                 result.append(node.word)
-                node.word = None 
+                node.word = None
 
-            board[r][c] = "#" 
-
-            for dr, dc in [(1,0), (-1,0), (0,1), (0,-1)]: 
+            board[r][c] = "#"
+            
+            directions = [
+                (1, 0), (-1, 0), (0, 1), (0, -1),
+                (1, 1), (1, -1), (-1, 1), (-1, -1)
+            ]
+            
+            for dr, dc in directions:
                 nr, nc = r + dr, c + dc
                 if 0 <= nr < rows and 0 <= nc < cols:
                     dfs(nr, nc, node)
 
-            board[r][c] = char 
+            board[r][c] = char
 
-        for r in range(rows):  
+        for r in range(rows):
             for c in range(cols):
                 dfs(r, c, trie.root)
 
-        return result  
-
-
+        return result
 
 solution = Solution()
 
@@ -62,4 +65,4 @@ board1 = [
     ["i","f","l","v"]
 ]
 words1 = ["oath", "pea", "eat", "rain"]
-print(solution.findWords(board1, words1)) 
+print(solution.findWords(board1, words1))
